83923001e8a61a0395a821388738f623f8c35a34
haejin <luckyhaejin1126@gmail.com>
2024-03-11 13:15:59 +0900

    리팩토링

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,13 +4,11 @@ require('dotenv').config();
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 authController.authenticate = (req,res,next) => {
     try {
-        const tokenString = req.headers.authorization; 
-        if(!tokenString) throw new Error("Invalid token");
+        const tokenString = req.headers.authorization;
+        if(!tokenString) throw new Error("헤더에 토큰이 존재하지 않습니다.");
         const token = tokenString.replace("Bearer ", "");
         jwt.verify(token, JWT_SECRET_KEY, (error, payload) =>{
-            if(error) {
-                throw new Error('Invalid token');
-            }
+            if(error) throw new Error('토큰이 유효하지 않습니다.');
             req.userId = payload._id;
         });
         next();
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -7,7 +7,6 @@ userController.createUser = async(req, res) => {
         const {name, email, password} = req.body;
         const user = await User.findOne({email});
         if(user) throw new Error('이미 가입이 된 유저입니다.');
-        if(!name || !email || !password) throw new Error('이름, 이메일, 비밀번호 등 필수 정보를 입력해주세요.');
         const salt = bcrypt.genSaltSync(saltRounds);
         const hash = bcrypt.hashSync(password, salt);
         const newUser = new User({name, email, password:hash});
@@ -22,14 +21,11 @@ userController.loginWithEmail = async(req, res) => {
     try {
         const {email,password} = req.body;
         const user = await User.findOne({email},"-createdAt -updatedAt -__v");
-        if(user) {
-            const isMatch = bcrypt.compareSync(password,user.password);
-            if(isMatch) {
-                const token = user.generateToken();
-                return res.status(200).json({status:"ok", user, token});
-            } 
-        }
-        throw new Error('아이디 또는 비밀번호가 일치하지 않습니다.');
+        if(!user) throw new Error('유저가 존재하지 않습니다.')
+        const isMatch = bcrypt.compareSync(password,user.password);
+        if(!isMatch) throw new Error('비밀번호가 일치하지 않습니다.');
+        const token = user.generateToken();
+        return res.status(200).json({status:"ok", user, token});    
     } catch (error) {
         res.status(400).json({status:'fail', message:error.message});
     }
